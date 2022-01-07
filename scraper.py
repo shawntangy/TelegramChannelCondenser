2296bc5a4c501e3e2dd04663efe306bdcce0716a
Shawn <38773103+shawntangy@users.noreply.github.com>
2022-01-07 20:48:45 +0800

    update scraper to allow checking of next n messages

--- scraper.py
+++ scraper.py
@@ -6,46 +6,54 @@ message_details = {"user1":
                        {"SGBIGPURCHASES": 2}
                    }
 
-
-def base_length(group_name):
-    url = "https://t.me/" + group_name + "/1"
+# to retrieve initial source code length
+def get_html(group_name, number):
+    url = "https://t.me/" + group_name + "/" + str(number)
     response = requests.get(url)
     html = response.text
-    return len(html)
+    return html
 
-
+# to retrieve actual content
 def scrape(message_details):
     for i in message_details:
         for key, value in message_details[i].items():
             group_name = key
             last_message = value
-    while True:
-        url = "https://t.me/" + group_name + "/" + str(last_message)
-        response = requests.get(url)
-        html = response.text
+            return poll(message_details, i, group_name, last_message)
 
-        if (len(html) - THRESHOLD >= 2000):
-            soup = BeautifulSoup(html, "html.parser")
+def poll(message_details, i, group_name, last_message):
+  while True:
+    html = get_html(group_name, last_message)
+    if (len(html) - THRESHOLD >= 2000):
+        soup = BeautifulSoup(html, "html.parser")
 
-            # to print message content
-            message = str(soup.select_one("meta[property=\"og:description\"]"))
-            # removes first 15 char, last 29
-            message = message[15:-29]
+        # to print message content
+        message = str(soup.select_one("meta[property=\"og:description\"]"))
+        # removes first 15 char, last 29
+        message = message[15:-29]
 
-            print(message)
-            # print(type(message))
-            last_message += 1
+        print(message)
+        # print(type(message))
+        last_message += 1
+        print("last message 39")
+        print(last_message)
+        # INCREMENT BACK INTO DICTIONARY NOT DONE
+    else:
+      for j in range(5):
+        if (len(get_html(group_name, last_message + j + 1)) - THRESHOLD < 2000):
+          print("checking last_message +"+ str(j+1))
+          pass
+        else: #to enter this - a message must exist
+          # this will be an actual message, hence update last_message
+          last_message = last_message + j + 1
+          message_details[i][group_name] = last_message 
+          print("scan complete, returning latest number")
+          return last_message
+      return last_message
+    message_details[i][group_name] = last_message 
 
-        else:
-            print("hello")
-            return last_message
 
-
-THRESHOLD = base_length("SGBIGPURCHASES")
-
-for i in message_details:
-    for key, value in message_details[i].items():
-        print(value)
+THRESHOLD = len(get_html("SGBIGPURCHASES", 1))
 
 last_message = scrape(message_details)
 
@@ -53,9 +61,6 @@ schedule.every(5).seconds.do(scrape, message_details)
 
 last_message = scrape(message_details)
 
-for i in message_details:
-    for key, value in message_details[i].items():
-        print(value)
-
 while True:
-    schedule.run_pending()
+  schedule.run_pending()
+
