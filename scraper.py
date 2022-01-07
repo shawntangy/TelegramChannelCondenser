d2ef8523c1e30dac36070078fcabd2b46ef40422
Keith Sim <38850626+keithsim-16@users.noreply.github.com>
2022-01-07 19:44:00 +0800

    Scraper Update

--- scraper.py
+++ scraper.py
@@ -1,21 +1,52 @@
+import schedule
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://t.me/SGResearchLobang/991"
-#url = "https://t.me/sgjobspoint/356"
+last_message = 2
 
-response = requests.get(url)
-html = response.text
-#print(html)
-#print(len(html))
 
-soup = BeautifulSoup(html, "html.parser")
+def base_length(group_name):
+    url = "https://t.me/" + group_name + "/1"
+    response = requests.get(url)
+    html = response.text
+    return len(html)
 
-# to print message content
-message = str(soup.select_one("meta[property=\"og:description\"]"))
-# removes first 15 char, last 29
-message = message[15:-29]
 
-print(message)
-print(type(message))
+def scrape(group_name, last_message):
+    while True:
+        url = "https://t.me/" + group_name + "/" + str(last_message)
+        response = requests.get(url)
+        html = response.text
 
+        if (len(html) - THRESHOLD >= 2000):
+            soup = BeautifulSoup(html, "html.parser")
+
+            # to print message content
+            message = str(soup.select_one("meta[property=\"og:description\"]"))
+            # removes first 15 char, last 29
+            message = message[15:-29]
+
+            print(message)
+            # print(type(message))
+            last_message += 1
+        else:
+            print("hello")
+            return last_message
+
+
+THRESHOLD = base_length("SGBIGPURCHASES")
+
+print(last_message)
+
+last_message = scrape("SGBIGPURCHASES", last_message)
+
+schedule.every(5).seconds.do(scrape, "SGBIGPURCHASES", last_message)
+
+last_message = scrape("SGBIGPURCHASES", last_message)
+
+print(last_message)
+
+while True:
+    schedule.run_pending()
+
+print(list(message_details.keys())[0])
